900965bdb8db3fc2dba01aea7738d399257dd5c5
mestihudson <mestihudson@gmail.com>
2020-02-04 19:55:08 +0000

    Add jest coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   coverageDirectory: 'coverage',
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  }
 }
